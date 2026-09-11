2f8b6ecec92b709b4f7891422eb5a8065dd45473
scp-682-joke <zhibek_slambekova@mail.ru>
2026-09-11 12:16:49 +0500

    getting started

---
+++ Cake.java
@@ -0,0 +1,10 @@
+public final class Cake{
+    private final String occasion;
+    private final int layers;
+    private final String flavor;
+    private final String frosting;
+    private final String filling;
+    private final String decoration;
+    private final String message;
+
+}
