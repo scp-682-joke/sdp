719c0eb1f78a299229a264628a92c3f61bd38f25
scp-682-joke <zhibek_slambekova@mail.ru>
2026-09-11 12:39:10 +0500

    coding Product(base)

--- Cake.java
+++ Cake.java
@@ -1,10 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public final class Cake{
-    private final String occasion;
-    private final int layers;
     private final String flavor;
+    private final int layers;
     private final String frosting;
-    private final String filling;
-    private final String decoration;
+    private final List<String> toppings;
     private final String message;
 
+    public Cake(String flavor, int layers, String frosting, List<String> toppings, String message) {
+        this.flavor = flavor;
+        this.layers = layers;
+        this.frosting = frosting;
+        this.toppings = new ArrayList<>(toppings);
+        this.message = message;
+    }
+
+    public String getFlavor(){ return flavor; }
+    public int getLayers(){ return layers; }
+    public String getFrosting(){return frosting; }
+    public List<String> getToppings(){ return Collections.unmodifiableList(toppings);}
+
+    @Override
+    public String toString() {
+        return "Cake {" +
+                "\n  Flavor: '" + flavor + '\'' +
+                "\n  Layers: " + layers +
+                "\n  Frosting: '" + frosting + '\'' +
+                "\n  Toppings: " + toppings +
+                "\n  Writing: '" + (message.isEmpty() ? "None" : message) + '\'' +
+                "\n}";
+    }
 }
